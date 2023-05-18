c74a6d50e7446fee684554340d1d6ba1e842ecc1
Indre <indre.surviliene@stud.vtmc.lt>
2023-05-18 12:49:47 +0300

    Initial commit

---
+++ src/test/java/com/myproject/WarehouseManagementApp/WarehouseManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.WarehouseManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
